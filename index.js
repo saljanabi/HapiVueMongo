8ba6b548083491e2737f7f90d7a8287d37c796e5
Sarah <sarah.aljanabi@gmail.com>
2018-09-21 14:40:04 +0200

    modified index

--- index.js
+++ index.js
@@ -1,52 +1,71 @@
+'use strict';
+
 const Hapi = require('hapi');
+const Config = require('config');
+
+const options = {
+    ops: {
+        interval: 1000
+    },
+    reporters: {
+        myConsoleReporter: [{
+            module: 'good-squeeze',
+            name: 'Squeeze',
+            args: [{
+                log: '*',
+                response: '*'
+            }]
+        }, {
+            module: 'good-console'
+        }, 'stdout']
+    }
+}
 
 const server = Hapi.server({
     port: 80,
-    load: { sampleInterval: 1000 },
+    load: {
+        sampleInterval: 1000
+    },
 });
+
+server.connection(Config.get('api'));
+
+server.ext({
+    type: 'onRequest',
+    method: function (request, h) {
+
+        // Change all requests to '/test'
+
+        request.setUrl('/test');
+        return h.continue;
+    }
+});
+
+server.register({
+    register: require('good'),
+    options,
+}, (err) => {
+    server.route({
+        method: 'GET',
+        path: '/',
+        handler: function (request, reply) => {
+            reply('Hello, world!');
+        }
+    })
+}
+    );
 
 const init = async () => {
-
     await server.start();
-    console.log( `Server running at: ${ server.info.uri }` );
+    console.log(`Server running at: ${server.info.uri}`);
 };
 
-server.start(err => {
+server.start((err) => {
 
     if (err) {
-
-        // Fancy error handling here
-        console.error( 'Error was handled!' );
-        console.error( err );
-
+        throw err;
     }
-
-    console.log( `Server started at ${ server.info.uri }` );
-
-});
-
-server.route({
-    method: 'GET',
-    path: '/',
-    handler: (request, h) => {
-
-        return 'Hello, world!';
-    }
-});
-
-server.route({
-    method: 'GET',
-    path: '/{name}',
-    handler: (request, h) => {
-
-        return 'Hello, ' + encodeURIComponent(request.params.name) + '!';
-    }
-});
-
-process.on('unhandledRejection', (err) => {
-
-    console.log(err);
-    process.exit(1);
-});
+    console.log(`Server running at: ${server.info.uri}`);
+};
 
 init();
